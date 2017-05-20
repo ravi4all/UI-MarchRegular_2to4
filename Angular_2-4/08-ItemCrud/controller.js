36bdf1e9cc86e4819275227ddccb4254d096747b
ravi4all <ravikanttyagi.2295@gmail.com>
2017-05-20 14:24:26 +0530

    Add files via upload

--- Angular_2-4/08-ItemCrud/controller.js
+++ Angular_2-4/08-ItemCrud/controller.js
@@ -1,7 +1,15 @@
 app.controller('itemCtrl', function($scope, itemFactory){
     $scope.add = function(){
-        var itemOperation = new Item(itemFactory.id, $scope.name, $scope.price)
+        var itemOperation = new Item(itemFactory.id, $scope.name,
+            $scope.price);
         itemFactory.addItem(itemOperation);
-        $scope.result = itemFactory.itemList;
+        $scope.result = itemFactory.getList();
     }
-})
+    $scope.countDeleted = function(){
+        return itemFactory.countDeleted();
+    }
+    $scope.delete = function(){
+        itemFactory.deleteItem();
+        $scope.result = itemFactory.getList();
+    }
+});
--- Angular_2-4/08-ItemCrud/factory.js
+++ Angular_2-4/08-ItemCrud/factory.js
@@ -2,6 +2,11 @@ function Item(id,name,price){
     this.id = id;
     this.name = name;
     this.price = price;
+    this.isDelete = false;
+    this.toggleDelete = function(){
+        this.isDelete = !this.isDelete;
+        console.log(this.isDelete);
+    }
 }
 
 app.factory('itemFactory',function(){
@@ -11,7 +16,20 @@ app.factory('itemFactory',function(){
         addItem : function(itemOperations){
             this.itemList.push(itemOperations)
             this.id ++;
-            console.log(this.itemList);
+            //console.log(this.itemList);
+        },
+        countDeleted : function(){
+            return this.itemList.filter(function(itemOperations){
+                return itemOperations.isDelete;
+            }).length;
+        },
+        deleteItem : function(){
+            this.itemList =  this.itemList.filter(function(itemOperation){
+                return !itemOperation.isDelete;
+            })
+        },
+        getList : function(){
+            return this.itemList;
         }
     }
     return itemObj;
